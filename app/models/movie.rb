b6811ba4dfe17d9df8c542ce1f1b4e16b458e717
VentoPreto <67896322+ventopreto@users.noreply.github.com>
2024-10-23 17:09:05 -0300

    cria o model, adiciona valições e testes

---
+++ app/models/movie.rb
@@ -0,0 +1,7 @@
+class Movie < ApplicationRecord
+  validates :title, presence: true, uniqueness: true
+  validates :genre, presence: true
+  validates :year, presence: true, numericality: {only_integer: true}
+  validates :country, presence: true
+  validates :published_at, presence: true
+end
---
+++ spec/models/movie_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+RSpec.describe Movie, type: :model do
+  context "Validações" do
+    it { should validate_presence_of(:title) }
+    it { should validate_uniqueness_of(:title) }
+    it { should validate_presence_of(:genre) }
+    it { should validate_presence_of(:year) }
+    it { should validate_numericality_of(:year).only_integer }
+    it { should validate_presence_of(:country) }
+    it { should validate_presence_of(:published_at) }
+  end
+end
