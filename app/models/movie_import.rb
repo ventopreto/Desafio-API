53dcc3dfd5a9f43560c9a374495ae88b9b457cca
VentoPreto <67896322+ventopreto@users.noreply.github.com>
2024-10-25 01:38:46 -0300

    refatora movie import e adiciona testes

--- app/models/movie_import.rb
+++ app/models/movie_import.rb
@@ -7,40 +7,45 @@ class MovieImport < ApplicationRecord
   enum status: {failed: 0, processing: 1, completed: 2, invalid_file: 3}
 
   def import_movies(file)
-    if valid_csv_file?(file)
-      movies_data = parse_csv_data(file)
+    return update_failed_import("Formato de arquivo inválido. Por favor, envie um arquivo CSV.", 3) unless valid_csv_file?(file)
+    return update_failed_import("Arquivo vazio. Por favor, insira um arquivo com dados dos filmes.", 3) unless empty_file?(file)
 
-      if movies_data.any?
-        Movie.insert_all(movies_data)
-        update(status: 2, movies_count: movies_data.size)
-      else
-        update(status: 0, movies_count: 0)
-      end
-    else
-      update(status: 3, movies_count: 0,
-        error_message: "Formato de arquivo inválido. Por favor, envie um arquivo CSV.")
-      false
+    begin
+      movies_data = create_movies(file)
+      update(status: 2, movies_count: movies_data.size) if movies_data.any?
+    rescue ActiveRecord::RecordInvalid => e
+      update_failed_import("Erro ao criar filmes: #{e.message}", 0)
     end
   end
 
   private
 
+  def update_failed_import(message, status)
+    update(status: status, movies_count: 0, error_message: message)
+    false
+  end
+
   def valid_csv_file?(file)
     file && file.content_type == "text/csv"
   end
 
-  def parse_csv_data(file)
+  def empty_file?(file)
+    require "csv"
+    CSV.readlines(file).any?
+  end
+
+  def create_movies(file)
     require "csv"
     CSV.readlines(file).map.with_index do |row, index|
       next if index == 0
-      {
+      Movie.create!(
         genre: row[1],
         title: row[2],
         country: row[5],
         published_at: row[6],
         year: row[7],
         description: row[11]
-      }
+      )
     end.compact
   end
 end
--- spec/models/movie_import_spec.rb
+++ spec/models/movie_import_spec.rb
@@ -5,34 +5,54 @@ RSpec.describe MovieImport, type: :model do
 
   let(:valid_file) { fixture_file_upload("spec/fixtures/valid_file.csv", "text/csv") }
   let(:empty_file) { fixture_file_upload("spec/fixtures/empty_file.csv", "text/csv") }
+  let(:with_missing_title) { fixture_file_upload("spec/fixtures/with_missing_title.csv", "text/csv") }
   let(:invalid_file) { fixture_file_upload("spec/fixtures/invalid_file.txt", "text/plain") }
 
-  it "processa filmes e atualiza o status para completed" do
-    movie_import = MovieImport.create(file_name: valid_file.original_filename, status: 1)
+  context "Quando o arquivo é válido" do
+    it "e os dados estão corretos processa filmes e atualiza o status para completed" do
+      movie_import = MovieImport.create(file_name: valid_file.original_filename, status: 1)
 
-    expect {
-      movie_import.import_movies(valid_file)
-    }.to change { Movie.count }.by(131)
+      expect {
+        movie_import.import_movies(valid_file)
+      }.to change { Movie.count }.by(131)
 
-    expect(movie_import.status).to eq("completed")
-    expect(movie_import.movies_count).to eq(131)
+      expect(movie_import.status).to eq("completed")
+      expect(movie_import.movies_count).to eq(131)
+    end
+
+    it "mas tem filmes sem titulo corretos processa filmes e atualiza o status para failed" do
+      movie_import = MovieImport.create(file_name: with_missing_title.original_filename, status: 1)
+
+      expect {
+        movie_import.import_movies(with_missing_title)
+      }.to change { Movie.count }.by(0)
+
+      expect(movie_import.status).to eq("failed")
+      expect(movie_import.movies_count).to eq(0)
+    end
   end
 
-  it "falha ao tentar importar arquivo inválido" do
-    movie_import = MovieImport.create(file_name: invalid_file.original_filename, status: 1)
+  context "Quando o arquivo é inválido" do
+    it "atualiza o status para invalid_file e retorna erro de arquivo inválido" do
+      movie_import = MovieImport.create(file_name: invalid_file.original_filename, status: 1)
 
-    movie_import.import_movies(invalid_file)
+      movie_import.import_movies(invalid_file)
 
-    expect(movie_import.status).to eq("invalid_file")
-    expect(movie_import.movies_count).to eq(0)
-  end
+      expect(movie_import.status).to eq("invalid_file")
+      expect(movie_import.error_message).to eq("Formato de arquivo inválido. Por favor, envie um arquivo CSV.")
+      expect(movie_import.movies_count).to eq(0)
+    end
 
-  it "falha ao tentar importar arquivo vazio" do
-    movie_import = MovieImport.create(file_name: empty_file.original_filename, status: 1)
+    context "Quando o arquivo é inválido" do
+      it "atualiza o status para invalid_file e retorna erro de arquivo vazio" do
+        movie_import = MovieImport.create(file_name: empty_file.original_filename, status: 1)
 
-    movie_import.import_movies(empty_file)
+        movie_import.import_movies(empty_file)
 
-    expect(movie_import.status).to eq("failed")
-    expect(movie_import.movies_count).to eq(0)
+        expect(movie_import.status).to eq("invalid_file")
+        expect(movie_import.error_message).to eq("Arquivo vazio. Por favor, insira um arquivo com dados dos filmes.")
+        expect(movie_import.movies_count).to eq(0)
+      end
+    end
   end
 end
